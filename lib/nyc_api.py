d4fa8dd32f875a8cca23eb163e58cc8f70592626
KELVIN MURITHI <kelvinmurithi@live.com>
2023-12-26 13:38:19 +0300

    changed url to wayback machine version, now trying to extract and parse the JSON data

--- lib/nyc_api.py
+++ lib/nyc_api.py
@@ -1,14 +1,25 @@
-
 import requests
+from bs4 import BeautifulSoup
 import json
 
 class GetPrograms:
 
   def get_programs(self):
-    URL = "http://data.cityofnewyork.us/resource/uvks-tn5n.json"
+    URL = "https://web.archive.org/web/20180303155827/https://data.cityofnewyork.us/resource/uvks-tn5n.json"
 
     response = requests.get(URL)
-    return response.content
+    soup = BeautifulSoup(response.content, 'html.parser')
+
+    # Print the prettified soup object
+    print(soup.prettify())
+
+    # Find the script tag containing the JSON data
+    script_tag = soup.find('script', {'type': 'application/json'})
+
+    # Extract and parse the JSON data
+    json_data = json.loads(script_tag.string)
+
+    return json_data
 
 programs = GetPrograms().get_programs()
 print(programs)
